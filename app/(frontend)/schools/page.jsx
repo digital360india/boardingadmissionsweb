03f544be2c43d1c643eb4e46c8e23d74f4e4dfc8
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-07-10 16:27:13 +0530

    final structure

---
+++ app/(frontend)/schools/page.jsx
@@ -0,0 +1,10 @@
+import SchoolsPage from '@/pages/frontend/SchoolsPage'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><SchoolsPage/></div>
+  )
+}
+
+export default page
---
+++ pages/frontend/SchoolsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SchoolsPage = () => {
+  return (
+    <div>SchoolsPage</div>
+  )
+}
+
+export default SchoolsPage
