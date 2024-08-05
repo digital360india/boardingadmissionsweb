8f5ae853fcb974d4dcb6c696fc17c1646a3460b2
Yogesh Pal <yogeshpal5049@gmail.com>
2024-08-05 12:03:29 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Barlow, Inter, Jost, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { Poppins } from "next/font/google";
-// import Navbar from "@/components/frontend/Navbar";
-// import Footer from "@/components/frontend/Footer";
+import Footer from "@/components/frontend/Footer";
+import Navbar from "@/components/frontend/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({
   subsets: ["latin"],
@@ -17,9 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Navbar/> */}
+      <Navbar/>
       <body className={`${poppins.variable}`}>{children}</body>{" "}
-      {/* <Footer/> */}
+      <Footer/>
     </html>
   );
 }
