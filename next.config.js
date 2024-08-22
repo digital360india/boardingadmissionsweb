3283bc350df6de85a0564f7227b49f61dbf574df
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-08-22 12:55:17 +0530

    new

---
+++ next.config.js
@@ -0,0 +1,12 @@
+export default {
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.js$/,
+        include: /node_modules/,
+        type: "javascript/auto",
+      });
+  
+      return config;
+    },
+  };
+  
