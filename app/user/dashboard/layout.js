ff59f84a0ac3d317721017d9e461f182bf479f2f
Himanshika Rawat <himanshirawat604@gmail.com>
2024-09-07 16:50:29 +0530

    dashboard

--- app/user/dashboard/layout.js
+++ app/user/dashboard/layout.js
@@ -1,12 +1,12 @@
-import { SideNavBar } from "@/components/backend/SideNavBar";
 import "../../globals.css";
 import { AdminProvider } from "@/adminProvider";
-import TopNavBar from "@/components/backend/TopNavBar";
 import { UserProvider } from "@/userProvider";
+import LayoutFile from "@/components/backend/LayoutFile";
 
 export const metadata = {
   title: "Get Admissions in Top Boarding Schools  | Admission 2025-26",
-  description: "Enroll in the best boarding schools for 2025-26. Give your child a top-quality education today!",
+  description:
+    "Enroll in the best boarding schools for 2025-26. Give your child a top-quality education today!",
 };
 
 export default function RootLayout({ children }) {
@@ -14,17 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       {" "}
       <UserProvider>
-        <div className="lg:flex">
-          <div className="w-1/5 hidden lg:block ">
-            <SideNavBar />
-          </div>
-          <div className="lg:flex flex-col lg:w-4/5">
-            <div className="h-16 ">
-              <TopNavBar />
-            </div>
-            <main className="min-h-screen md:mt-16 ">{children}</main>
-          </div>
-        </div>
+        <LayoutFile children={children} />
       </UserProvider>
     </html>
   );
