31720e4c168b4ae527c21223c291e7e250944a12
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-09-10 17:18:47 +0530

    anew

---
+++ app/admin/dashboard/leads/page.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const page = () => {
+  const [leads, setLeads] = useState([]);
+  const fetchData = async () => {
+    const leadsSnap = await getDocs(collection(db, "leads"));
+    const leadList = leadsSnap.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setLeads(leadList);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const deleteLead = async (id) => {
+    await deleteDoc(doc(db, "schools", id));
+    fetchData();
+  };
+  const handleEditLead = (id) => {
+    setEditingLead(id);
+    setIsDialogOpen(true);
+  };
+  return <div>page</div>;
+};
+
+export default page;
