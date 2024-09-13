ce3ddb43b57bd891dbb6d2f2a0cc2c4d348057bd
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-09-13 15:54:05 +0530

    PSUH

--- pages/backend/MyTestScreen.jsx
+++ pages/backend/MyTestScreen.jsx
@@ -11,7 +11,7 @@ const MyTestScreen = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
+
     const fetchPackages = async () => {
       console.log("User object:", user); 
 
@@ -42,7 +42,7 @@ const MyTestScreen = () => {
         setLoading(false);
       }
     };
-
+    useEffect(() => {
     fetchPackages();
   }, [user]);
 
@@ -50,54 +50,56 @@ const MyTestScreen = () => {
   if (error) return <div>{error}</div>;
 
   return (
+   <>      {user ? 
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6 text-background04 ">
-        My Packages
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
-        {packages.length > 0 ? (
-          packages.map((pkg) => (
-            <div
-              key={pkg.id}
-              className="bg-white shadow-lg rounded-lg overflow-hidden"
-            >
-              <img
-                src={pkg.image}
-                alt={pkg.packageName}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h2 className="text-xl font-semibold text-background04">
-                  {pkg.packageName}
-                </h2>
-                <p className="text-gray-700 mt-2">{`Starting Date: ${new Date(
-                  pkg.startingDate
-                ).toLocaleDateString()}`}</p>
-                <p className="text-gray-700 mt-1">{`Date of Creation: ${new Date(
-                  pkg.dateOfCreation
-                ).toLocaleDateString()}`}</p>
-                <p className="text-gray-700 mt-1">{`Students Enrolled: ${pkg.studentsEnrolled}`}</p>
-                <p className="text-gray-700 mt-1">{`Price: ₹${pkg.price}`}</p>
-                <p className="text-gray-700 mt-1">{`Discounted Price: ₹${pkg.discountedPrice}`}</p>
-                <div className="mt-4 flex w-full justify-between items-center">
-                  <Link
-                    className="flex w-full"
-                    href={`/user/dashboard/mytests/[mytests]`}
-                    as={`/user/dashboard/mytests/${pkg.id}`}
-                  >
-                    <button className="bg-background04 text-white px-4 py-2 rounded-lg w-full">
-                      Learn
-                    </button>
-                  </Link>
-                </div>
+    <h1 className="text-3xl font-bold mb-6 text-background04 ">
+      My Packages
+    </h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+      {packages.length > 0 ? (
+        packages.map((pkg) => (
+          <div
+            key={pkg.id}
+            className="bg-white shadow-lg rounded-lg overflow-hidden"
+          >
+            <img
+              src={pkg.image}
+              alt={pkg.packageName}
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h2 className="text-xl font-semibold text-background04">
+                {pkg.packageName}
+              </h2>
+              <p className="text-gray-700 mt-2">{`Starting Date: ${new Date(
+                pkg.startingDate
+              ).toLocaleDateString()}`}</p>
+              <p className="text-gray-700 mt-1">{`Date of Creation: ${new Date(
+                pkg.dateOfCreation
+              ).toLocaleDateString()}`}</p>
+              <p className="text-gray-700 mt-1">{`Students Enrolled: ${pkg.studentsEnrolled}`}</p>
+              <p className="text-gray-700 mt-1">{`Price: ₹${pkg.price}`}</p>
+              <p className="text-gray-700 mt-1">{`Discounted Price: ₹${pkg.discountedPrice}`}</p>
+              <div className="mt-4 flex w-full justify-between items-center">
+                <Link
+                  className="flex w-full"
+                  href={`/user/dashboard/mytests/[mytests]`}
+                  as={`/user/dashboard/mytests/${pkg.id}`}
+                >
+                  <button className="bg-background04 text-white px-4 py-2 rounded-lg w-full">
+                    Learn
+                  </button>
+                </Link>
               </div>
             </div>
-          ))
-        ) : (
-          <p>No course packages available.</p>
-        )}
-      </div>
+          </div>
+        ))
+      ) : (
+        <p>No course packages available.</p>
+      )}
     </div>
+    </div> : <div>Loading...</div>}
+</>
   );
 };
 
