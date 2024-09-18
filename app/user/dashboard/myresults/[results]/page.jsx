56f429420b62a8e7aab713aa475428525a85bfd8
shanuchauhan1211 <shanu1211chauhan@gmail.com>
2024-09-18 16:52:14 +0530

    change done

--- app/user/dashboard/myresults/[results]/page.jsx
+++ app/user/dashboard/myresults/[results]/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState } from 'react';
-export default function page()
+export default function Page()
 {
 const [ showAttemptedques, setShowAttemptedques] = useState(true);
 
