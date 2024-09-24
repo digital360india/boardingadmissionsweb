c7afdcf0ac77ceb3998e1f1a6ce985a08dc100a6
Yogesh Pal <yogeshpal5049@gmail.com>
2024-09-24 13:28:51 +0530

    image changes

--- components/frontend/AboutMakeUs.jsx
+++ components/frontend/AboutMakeUs.jsx
@@ -3,17 +3,18 @@ import React from "react";
 
 export default function AboutMakeUs() {
   return (
-    <div className="flex h-full w-full">
-      <div className="lg:block hidden">
+    <div className="flex h-full w-full ">
+      <div className="lg:block hidden ">
         <Image
-          src="/images/aboutgirl.svg"
-          width={700}
-          height={700}
+          src="https://firebasestorage.googleapis.com/v0/b/boardingadmissions-f3ba3.appspot.com/o/images%2Faboutmakeusdifferent.png?alt=media&token=496c1d44-4e2b-4c30-9ada-93b011836489"
+          width={1000}
+          height={1000}
           alt="Image"
+          className="w-full h-full object-cover"
         />
       </div>
 
-      <div className="bg-[#F4FCFC] ">
+      <div className="bg-[#F4FCFC] w-full ">
         <div>
           <p className="text-primary02 lg:text-32px lg:text-center text-[28px] font-medium lg:font-semibold lg:my-2 lg:px-[0] px-[24px] my-3">
             What Makes us Different
@@ -27,127 +28,86 @@ export default function AboutMakeUs() {
 
         <div className="lg:hidden flex justify-center my-4 px-[24px]">
           <Image
-            src="/images/aboutgirl2.svg"
+            src="https://firebasestorage.googleapis.com/v0/b/boardingadmissions-f3ba3.appspot.com/o/images%2Faboutmakeusdifferent.png?alt=media&token=496c1d44-4e2b-4c30-9ada-93b011836489"
             className="w-full "
-            width={1}
-            height={1}
+            width={1000}
+            height={1000}
             alt="Image"
           />
         </div>
 
-       {/* Component for Small Screens */}
-<div className="flex items-center justify-center mt-2 lg:hidden">
-  <div className="relative w-[90vw] xl:w-[35vw] h-auto xl:h-[38vh] lg:h-[27vh] lg:w-[40vw] grid grid-cols-2 grid-rows-2 gap-y-4 lg:gap-y-8">
-    {/* Background Image */}
-    <Image
-      src="/images/lines.svg"
-      width={2}
-      height={2}
-      alt="Image"
-      className="absolute inset-0 xl:w-[540px] xl:h-[450px] lg:w-[400px] lg:h-[270px] h-full w-full object-contain"
-    />
+        {/* Component for Small Screens */}
+        <div className="flex items-center justify-center mt-2 lg:hidden">
+          <div className="relative w-[90vw] xl:w-[35vw] h-auto xl:h-[38vh] lg:h-[27vh] lg:w-[40vw] grid grid-cols-2 grid-rows-2 gap-y-4 lg:gap-y-8">
+            {/* Background Image */}
+            <Image
+              src="/images/lines.svg"
+              width={2}
+              height={2}
+              alt="Image"
+              className="absolute inset-0 xl:w-[540px] xl:h-[450px] lg:w-[400px] lg:h-[270px] h-full w-full object-contain"
+            />
 
-    {/* First Column - Top Left */}
-    <div className="relative text-center lg:text-left p-2 xl:pt-4">
-      <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] pb-1 lg:pb-4">
-        <span className="lg:hidden block">
-          High <br /> Success
-        </span>
-        <span className="lg:block hidden">High Success</span>
-      </p>
-      <p className="text-[14px] md:text-[16px] w-[80%] md:w-full mx-auto lg:mx-0">
-        90% admission success.
-      </p>
-    </div>
+            {/* First Column - Top Left */}
+            <div className="relative text-center lg:text-left p-2 xl:pt-4">
+              <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] pb-1 lg:pb-4">
+                <span className="lg:hidden block">
+                  High <br /> Success
+                </span>
+                <span className="lg:block hidden">High Success</span>
+              </p>
+              <p className="text-[14px] md:text-[16px] w-[80%] md:w-full mx-auto lg:mx-0">
+                90% admission success.
+              </p>
+            </div>
 
-    {/* Second Column - Top Right */}
-    <div className="relative text-center lg:text-left p-2 xl:pt-4">
-      <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] pb-1 lg:pb-4">
-        <span className="lg:hidden block">
-          Expert <br /> Guidance
-        </span>
-        <span className="lg:block hidden">Expert Guidance</span>
-      </p>
-      <p className="text-[14px] md:text-[16px]">
-        Tailored entrance exam prep.
-      </p>
-    </div>
+            {/* Second Column - Top Right */}
+            <div className="relative text-center lg:text-left p-2 xl:pt-4">
+              <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] pb-1 lg:pb-4">
+                <span className="lg:hidden block">
+                  Expert <br /> Guidance
+                </span>
+                <span className="lg:block hidden">Expert Guidance</span>
+              </p>
+              <p className="text-[14px] md:text-[16px]">
+                Tailored entrance exam prep.
+              </p>
+            </div>
 
-    {/* Third Column - Bottom Left */}
-    <div className="relative text-center lg:text-left p-2 xl:pt-4">
-      <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] pb-1 lg:pb-4">
-        Holistic Approach
-      </p>
-      <p className="text-[14px] md:text-[16px] w-[160px] md:w-full">
-        Focus on all-round readiness.
-      </p>
-    </div>
+            {/* Third Column - Bottom Left */}
+            <div className="relative text-center lg:text-left p-2 xl:pt-4">
+              <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] pb-1 lg:pb-4">
+                Holistic Approach
+              </p>
+              <p className="text-[14px] md:text-[16px] w-[160px] md:w-full">
+                Focus on all-round readiness.
+              </p>
+            </div>
 
-    {/* Fourth Column - Bottom Right */}
-    <div className="relative text-center lg:text-left p-2 xl:pt-4">
-      <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] pb-1 lg:pb-4">
-        Customized Courses
-      </p>
-      <p className="text-[14px] md:text-[16px]">
-        Specific to top schools.
-      </p>
-    </div>
-  </div>
-</div>
+            {/* Fourth Column - Bottom Right */}
+            <div className="relative text-center lg:text-left p-2 xl:pt-4">
+              <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] pb-1 lg:pb-4">
+                Customized Courses
+              </p>
+              <p className="text-[14px] md:text-[16px]">
+                Specific to top schools.
+              </p>
+            </div>
+          </div>
+        </div>
 
-{/* Component for Larger Screens */}
-<div className="hidden lg:flex items-center justify-center mt-2">
-  <div className="relative w-[90vw] xl:w-[35vw] h-auto xl:h-[38vh] lg:h-[27vh] lg:w-[40vw]">
-   
-<Image
-      src="/images/Frame 50 (2).svg"
-      width={2}
-      height={2}
-      alt="Image"
-      className=" xl:h-[400px] w-[1200px] h-[250px] "
-    />
-    {/* <div className="absolute top-[10%] xl:top-[50px] lg:top-[0%] left-[8%] md:left-[10%] lg:left-[5%] lg:text-left md:w-[40%] lg:w-[40%]">
-      <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] xl:pb-4 lg:pb-1 pb-4">
-        High Success
-      </p>
-      <p className="text-[14px] w-[80%] md:w-full md:text-[16px] text-center lg:text-start">
-        90% admission success.
-      </p>
-    </div>
-
- 
-    <div className="absolute top-[10%] xl:top-[50px] lg:top-[0%] left-[50%] md:left-[55%] xl:left-[300px] lg:left-[240px] text-center lg:text-left md:w-[40%] lg:w-[40%] w-[50%]">
-      <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] xl:pb-4 lg:pb-1 pb-4">
-        Expert Guidance
-      </p>
-      <p className="text-[14px] md:text-[16px]">
-        Tailored entrance exam prep.
-      </p>
-    </div>
-
- 
-    <div className="absolute top-[55%] left-[3%] lg:top-[110%] xl:top-[240px] md:left-[10%] lg:left-[5%] text-center lg:text-left md:w-[40%] lg:w-[40%]">
-      <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] xl:pb-4 lg:pb-1 pb-4">
-        Holistic Approach
-      </p>
-      <p className="text-[14px] w-[160px] md:w-full md:text-[16px]">
-        Focus on all-round readiness.
-      </p>
-    </div>
-
-  
-    <div className="absolute top-[55%] lg:top-[110%] xl:top-[240px] left-[50%] md:left-[55%] xl:left-[300px] lg:left-[240px] text-center lg:text-left md:w-[40%] lg:w-[44%] w-[50%]">
-      <p className="text-primary02 text-[20px] md:text-[24px] lg:text-[28px] xl:pb-4 lg:pb-1 pb-4">
-        Customized Courses
-      </p>
-      <p className="text-[14px] md:text-[16px]">
-        Specific to top schools.
-      </p>
-    </div> */}
-  </div>
-</div>
-
-
+        {/* Component for Larger Screens */}
+        <div className="hidden lg:flex items-center justify-center ">
+          <div className="relative w-[90vw] xl:w-[32vw] h-auto xl:h-[25vh] lg:h-[27vh] lg:w-[40vw]">
+            <Image
+              src="/images/Frame 50 (2).svg"
+              width={1000}
+              height={1000}
+              alt="Image"
+              className=" xl:h-[315px] w-[1100px] h-[200px] "
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/frontend/AboutSuccess.jsx
+++ components/frontend/AboutSuccess.jsx
@@ -3,44 +3,49 @@ import React from "react";
 
 export default function AboutSuccess() {
   return (
-    <div className="bg-[#C7D5DC] lg:h-[683px] h-full lg:pb-0 pb-16 flex justify-between w-full xl:px-[100px] lg:px-[40px] px-[24px]">
-<div className="w-full lg:w-[46%]">
-  <p className="text-[#666666] text-16px lg:text-24px mt-12">
-  Success Starts Here
-  </p>
-  <p className="w-[90%] lg:w-[608px] text-32px lg:text-[48px] font-medium lg:pb-0 pb-5">
-    Your Success is our Top Priority
-  </p>
+    <div className="bg-[#C7D5DC] lg:h-[550px] h-full lg:pb-0 pb-16 flex justify-between w-full xl:px-[100px] lg:px-[40px] px-[24px]">
+      <div className="w-full lg:w-[46%]">
+        <p className="text-[#666666] text-16px lg:text-24px mt-12">
+          Success Starts Here
+        </p>
+        <p className="w-[90%]  lg:w-[510px] text-32px lg:text-[48px] font-medium lg:pb-0 pb-5">
+          Your Success is our Top Priority
+        </p>
 
-  <div className="lg:hidden flex justify-center items-center w-full">
-    <Image
-      src="/images/aboutsuccess2.svg"
-      width={380}
-      height={380}
-      className="w-full"
-      alt="Image"
-    />
-  </div>
+        <div className="lg:hidden flex justify-center items-center w-full">
+          <Image
+            src="https://firebasestorage.googleapis.com/v0/b/boardingadmissions-f3ba3.appspot.com/o/images%2Faboutsuccess.png?alt=media&token=deab4dd2-534c-4933-9200-13611b21eb9e"
+            width={1000}
+            height={1000}
+            className="w-full h-full"
+            alt="Image"
+          />
+        </div>
 
-  <div className="w-full">
-    <p className="text-18px my-6">
-    We provide expert guidance and tailored courses to ensure students are fully prepared for India&apos;s top boarding school entrance exams. With a deep understanding of elite school requirements, we enhance students&apos; chances of admission, setting the stage for their academic success and personal growth.
-    </p>
-   
-    {/* <button className="px-6 lg:px-16 py-2 text-primary02 bg-white mt-12 rounded-md border border-primary02">
+        <div className="w-full">
+          <p className="text-18px my-6">
+            We provide expert guidance and tailored courses to ensure students
+            are fully prepared for India&apos;s top boarding school entrance
+            exams. With a deep understanding of elite school requirements, we
+            enhance students&apos; chances of admission, setting the stage for
+            their academic success and personal growth.
+          </p>
+
+          {/* <button className="px-6 lg:px-16 py-2 text-primary02 bg-white mt-12 rounded-md border border-primary02">
       Primary
     </button> */}
-  </div>
-</div>
+        </div>
+      </div>
 
-      <div className="hidden lg:flex justify-center items-center">
+      <div className="hidden lg:flex justify-center mt-14">
         <Image
-          src="/images/aboutsuccess.svg"
-          width={500}
-          height={480}
+          src="https://firebasestorage.googleapis.com/v0/b/boardingadmissions-f3ba3.appspot.com/o/images%2Faboutsuccess.png?alt=media&token=deab4dd2-534c-4933-9200-13611b21eb9e"
+          width={1000}
+          height={1000}
           alt="Image"
+          className="w-[25rem]  h-[28rem] object-cover"
         />
       </div>
-    </div>
+    </div> 
   );
 }
