15418e0f017068b19a86ba99435d713a1ed06334
virajpant <vpant9211@gmail.com>
2024-09-26 15:10:38 +0530

    done changes

--- components/frontend/Flyer.jsx
+++ components/frontend/Flyer.jsx
@@ -9,7 +9,7 @@ export default function Flyer() {
   useEffect(() => {
     const interval = setInterval(() => {
       setShowFlyer(true);
-    }, 1500); 
+    }, 1500);
 
     return () => clearInterval(interval);
   }, []);
@@ -26,12 +26,10 @@ export default function Flyer() {
     };
   }, [showFlyer]);
 
-
   const handleClick = () => {
-    setShowFlyer(false); 
+    setShowFlyer(false);
     setPopupVisible(true);
   };
-
 
   const handleClosePopup = () => {
     setPopupVisible(false);
@@ -40,33 +38,63 @@ export default function Flyer() {
   return (
     <div>
       {showFlyer && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 backdrop-blur-sm z-50">
-          <div className=" rounded-lg shadow-lg relative lg:w-[40%] lg:h-[80%] w-[85%] md:h-[80%]">
-            <img
-              src="/gif/mobileflyer.gif"
-              alt="Mobile Flyer"
-              className="w-full h-full rounded-lg"
-            />
-         
-            <button
-              className="absolute top-1 right-1 bg-primary02 rounded-full text-white hover:text-gray-800 flex items-center justify-center lg:w-10 lg:h-10 w-8 h-8"
-              onClick={() => setShowFlyer(false)}
-            >
-              <img src="/Crossbutton.svg" className="text-6xl" alt="Close" />
-            </button>
+        <div>
+          <div className="md:hidden fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 backdrop-blur-sm z-50">
+            <div className=" rounded-lg shadow-lg relative lg:w-[40%] lg:h-[80%] w-[85%] md:h-[80%]">
+              <img
+                src="/images/mobileflyer.jpg"
+                alt="Mobile Flyer"
+                className="w-full h-full rounded-lg"
+              />
 
-            <div className="flex justify-center">
-  <button
-    className="relative bg-primary02 py-2 px-4 font-medium text-white rounded-b-2xl overflow-hidden group transition-colors duration-300 group-hover:bg-white"
-    onClick={handleClick}
-  >
-    <span className="relative inline-block">
-      Get in touch
-      <span className="absolute left-0 bottom-0 h-[2px] w-full bg-white transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
-    </span>
-  </button>
-</div>
+              <button
+                className="absolute top-1 right-1 bg-primary02 rounded-full text-white hover:text-gray-800 flex items-center justify-center lg:w-10 lg:h-10 w-8 h-8"
+                onClick={() => setShowFlyer(false)}
+              >
+                <img src="/Crossbutton.svg" className="text-6xl" alt="Close" />
+              </button>
 
+              <div className="flex justify-center">
+                <button
+                  className="relative bg-primary02 py-2 px-4 font-medium text-white rounded-b-2xl overflow-hidden group transition-colors duration-300 group-hover:bg-white"
+                  onClick={handleClick}
+                >
+                  <span className="relative inline-block">
+                    Get in touch
+                    <span className="absolute left-0 bottom-0 h-[2px] w-full bg-white transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="hidden fixed inset-0 md:flex items-center justify-center bg-black bg-opacity-75  backdrop-blur-sm z-50">
+            <div className=" rounded-xl shadow-lg relative  w-[85%] h-[80%] ">
+              <img
+                src="/images/dekstopflyer.jpg"
+                alt="dekstop Flyer"
+                className="w-full h-full"
+              />
+
+              <button
+                className="absolute top-1 right-1 bg-primary02 rounded-full text-white hover:text-gray-800 flex items-center justify-center lg:w-10 lg:h-10 w-8 h-8"
+                onClick={() => setShowFlyer(false)}
+              >
+                <img src="/Crossbutton.svg" className="text-6xl" alt="Close" />
+              </button>
+
+              <div className="flex justify-center">
+                <button
+                  className="relative bg-primary02 py-2 px-4 font-medium text-white rounded-b-2xl overflow-hidden group transition-colors duration-300 group-hover:bg-white"
+                  onClick={handleClick}
+                >
+                  <span className="relative inline-block">
+                    Get in touch
+                    <span className="absolute left-0 bottom-0 h-[2px] w-full bg-white transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
+                  </span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       )}
