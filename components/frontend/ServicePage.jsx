f045ae1c0c0d9b4237ff2dd70a5bf41d33383bbf
Praveen <praveennegi700@gmail.com>
2025-02-28 17:53:19 +0530

    Refactor ServicesHero component: update heading text, improve paragraph styling, and enhance button design

--- components/frontend/ServicePage.jsx
+++ components/frontend/ServicePage.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
@@ -18,12 +18,12 @@ const ServicePage = () => {
     <>
       <div>
         <div className="">
-          <div className="mx-4 mt-2 md:mx-20 md:mt-14">
-            <div id="onlineclass">
-              <h1 className="text-[1.5rem] font-bold md:text-[3.5rem] text-primary02">
-                ONLINE CLASS
+          <div className="mx-4 mt-2 md:mx-20 md:mt-14 md:flex md:justify-between md:items-center">
+            <div id="onlineclass" className="md:w-1/2">
+              <h1 className="lg:text-[48px] text-[32px]  md:text-[3.5rem] text-primary02">
+                Online Class
               </h1>
-              <p className="text-[0.875rem] md:text-[18px] text-[#010101] mt-4">
+              <p className="text-[0.875rem] md:text-[20px] text-[#010101] mt-4">
                 Experience engaging and personalized online learning with
                 Boarding Admissions. Our state-of-the-art online classes offer
                 flexibility and convenience, allowing students to learn from
@@ -43,7 +43,7 @@ const ServicePage = () => {
                   className="w-[638px] h-[280px]"
                 />
               </div>
-              <div>
+              {/* <div>
                 <Image
                   src="/icons/online2.svg"
                   width={1000}
@@ -51,8 +51,8 @@ const ServicePage = () => {
                   alt="course1"
                   className="w-[368px] h-[280px]"
                 />
-              </div>
-              <div className=" space-y-3 md:hidden xl:block">
+              </div> */}
+              {/* <div className=" space-y-3 md:hidden xl:block">
                 <div>
                   <Image
                     src="/icons/online3.svg"
@@ -69,10 +69,9 @@ const ServicePage = () => {
                     height={1000}
                     alt="course1"
                     className="w-[187px] h-[131px]"
-
                   />
                 </div>
-              </div>
+              </div> */}
             </div>
 
             <div className="md:hidden lg:hidden mt-4">
@@ -81,64 +80,64 @@ const ServicePage = () => {
                 width={1000}
                 height={280}
                 alt="course1"
-                
               />
             </div>
           </div>
 
           <hr className="mt-5 md:mt-12" />
-          <div className="mx-4 mt-4 md:mt-8 md:mx-20">
-            <div className=" " id="personalinterview">
-              <h1 className="text-[1.5rem] font-bold md:text-[3.5rem] text-primary02">
-                PERSONAL INTERVIEW PREPARATION
-              </h1>
-              <p className="text-[0.875rem] md:text-[18px] text-[#010101] mt-4">
-                Master the personal interview for boarding school admissions
-                with Boarding Admissions. Our program helps students showcase
-                their personality, interests, and potential confidently. We
-                offer expert guidance on interview techniques, interactive mock
-                interviews, and personalized feedback to improve performance.
-                Our goal is to build students confidence and prepare them
-                thoroughly for their interviews.
-              </p>
-            </div>
-            <div className="hidden   md:flex md:space-x-12 md:mt-4">
-              <div>
+          <div className="bg-gradient-to-br from-[#075D70] to-[#0DB2D6]">
+            <div className="mx-4 mt-4 md:mt-8 md:px-20 md:flex md:justify-between md:space-x-14  md:pt-12 md:pb-12 w-[100%]">
+              <div className="hidden md:flex md:justify-between  md:space-x-6 w-[50%]">
                 <Image
                   src="/icons/PI1.svg"
                   width={1000}
                   height={1000}
                   alt="course1"
-                  className="w-[632px] h-[280px]"
+                  className="w-full h-full"
                 />
+                {/* <Image
+                    src="/icons/PI2.svg"
+                    width={1000}
+                    height={1000}
+                    alt="course2"
+                    className="w-[300px] h-[200px]"
+                  /> */}
               </div>
-              <div>
+
+              <div id="personalinterview" className="w-[50%]">
+                <h1 className="lg:text-[44px] text-[28px] text-white">
+                  Personal Interview Preparation
+                </h1>
+                <p className="text-[0.875rem] md:text-[20px] leading-relaxed text-white mt-4">
+                  Master the personal interview for boarding school admissions
+                  with Boarding Admissions. Our program helps students showcase
+                  their personality, interests, and potential confidently. We
+                  offer expert guidance on interview techniques, interactive
+                  mock interviews, and personalized feedback to improve
+                  performance. Our goal is to build students' confidence and
+                  prepare them thoroughly for their interviews.
+                </p>
+              </div>
+
+              <div className="md:hidden lg:hidden mt-4">
                 <Image
-                  src="/icons/PI2.svg"
+                  src="/icons/PI1.svg"
                   width={1000}
-                  height={1000}
+                  height={280}
                   alt="course1"
-                  className="w-[579px] h-[280px]"
                 />
               </div>
-            </div>
-            <div className="md:hidden lg:hidden mt-4">
-              <Image
-                src="/icons/PI1.svg"
-                width={1000}
-                height={280}
-                alt="course1"
-              />
             </div>
           </div>
+
           <hr className="mt-5 md:mt-12" />
 
-          <div className="mx-4 mt-4 md:mt-8 md:mx-20">
-            <div className="" id="mocktest">
-              <h1 className="text-[1.5rem] font-bold md:text-[3.5rem] text-primary02">
-                MOCK TEST
+          <div className="mx-4 mt-4 md:mt-8 md:mx-20 md:flex md:justify-between md:items-center">
+            <div className="md:w-1/2" id="mocktest">
+              <h1 className="lg:text-[48px] text-[32px] text-primary02">
+                Mock Test
               </h1>
-              <p className="text-[0.875rem] md:text-[18px] text-[#010101] mt-4">
+              <p className="text-[0.875rem] md:text-[20px] text-[#010101] mt-4">
                 Mock tests are vital for boarding school entrance exam prep. At
                 Boarding Admissions, our comprehensive tests simulate the actual
                 exam environment, helping students reduce anxiety and build
@@ -159,7 +158,7 @@ const ServicePage = () => {
                   className="w-[632px] h-[280px]"
                 />
               </div>
-              <div>
+              {/* <div>
                 <Image
                   src="/icons/MT2.svg"
                   width={1000}
@@ -176,7 +175,7 @@ const ServicePage = () => {
                   alt="Mock Test"
                   className="w-[280px] h-[280px]"
                 />
-              </div>
+              </div> */}
             </div>
             <div className="md:hidden lg:hidden mt-4">
               <Image
@@ -188,70 +187,72 @@ const ServicePage = () => {
             </div>
           </div>
           <hr className="mt-5 md:mt-12" />
-          <div className="mx-4 mt-4 md:mt-8 md:mx-20">
-            <div className="" id="doubtclear">
-              <h1 className="text-[1.5rem] font-bold md:text-[3.5rem] text-primary02">
-                DOUBT CLEARING SESSION
-              </h1>
-              <p className="text-[0.875rem] md:text-[18px] text-[#010101] mt-4">
-                Our doubt-clearing sessions are designed to address students
-                specific challenges in preparation for boarding school entrance
-                exams. These interactive sessions offer personalized guidance,
-                helping students overcome difficulties and solidify their
-                understanding. With expert instructors available to answer
-                questions and provide clear explanations, students can resolve
-                their doubts efficiently. Regular doubt-clearing sessions ensure
-                that no question goes unanswered, boosting students confidence
-                and readiness for the exams.
-              </p>
-            </div>
-            <div className="hidden    md:flex md:space-x-12 md:mt-4">
-              <div>
-                <Image
-                  src="/icons/Doubt1.svg"
-                  width={1000}
-                  height={1000}
-                  alt="Doubt1"
-                  className="w-[632px] h-[280px]"
-                />
-              </div>
-              <div>
-                <Image
-                  src="/icons/Doubt2.svg"
-                  width={1000}
-                  height={1000}
-                  alt="Doubt2"
-                  className="w-[368px] h-[280px]"
-                />
-              </div>
+          <div className="bg-gradient-to-br from-[#075D70] to-[#0DB2D6] mt-8">
+            <div className="bg-gradient-to-br from-[#075D70] to-[#0DB2D6] overflow-hidden">
+              <div className="mx-4 mt-4 md:mt-8 md:mx-20 md:flex md:justify-between md:items-center md:space-x-12 md:pt-12 md:pb-12">
+                <div className="hidden md:flex md:w-1/2 md:justify-start">
+                  <Image
+                    src="/icons/Doubt1.svg"
+                    width={1000}
+                    height={1000}
+                    alt="Doubt1"
+                    className="max-w-full h-auto object-contain"
+                  />
 
-              <div>
-                <Image
-                  src="/icons/Doubt3.svg"
-                  width={1000}
-                  height={1000}
-                  alt="Doubt3"
-                  className="w-[187px] h-[280px]"
-                />
+                  {/* <div>
+                    <Image
+                      src="/icons/Doubt2.svg"
+                      width={1000}
+                      height={1000}
+                      alt="Doubt2"
+                      className="w-[368px] h-[280px]"
+                    />
+                  </div>
+
+                  <div>
+                    <Image
+                      src="/icons/Doubt3.svg"
+                      width={1000}
+                      height={1000}
+                      alt="Doubt3"
+                      className="w-[187px] h-[280px]"
+                    />
+                  </div> */}
+                </div>
+
+                <div className="md:w-1/2">
+                  <h1 className="lg:text-[44px] text-[28px] text-white">
+                    Doubt Clearing Session
+                  </h1>
+                  <p className="text-[0.875rem] md:text-[20px] leading-relaxed text-white mt-4">
+                    Our doubt-clearing sessions are designed to address
+                    students' specific challenges in preparation for boarding
+                    school entrance exams. These interactive sessions offer
+                    personalized guidance, helping students overcome
+                    difficulties and solidify their understanding. With expert
+                    instructors available to answer questions and provide clear
+                    explanations, students can resolve their doubts efficiently.
+                    Regular doubt-clearing sessions ensure that no question goes
+                    unanswered, boosting students' confidence and readiness for
+                    the exams.
+                  </p>
+                </div>
+
+                <div className="md:hidden lg:hidden mt-4">
+                  <Image
+                    src="/icons/Doubt1.svg"
+                    width={1000}
+                    height={280}
+                    alt="Doubt1"
+                    className="max-w-full h-auto object-contain"
+                  />
+                </div>
               </div>
-            </div>
-            <div className="md:hidden lg:hidden mt-4">
-              <Image
-                src="/icons/Doubt1.svg"
-                width={1000}
-                height={280}
-                alt="Doubt1"
-              />
             </div>
           </div>
           <hr className="mt-5 md:mt-12" />
 
-
-
-
-          {isPopupVisible && (
-                  <BookaDemoPopUp onClose={handleClosePopup} />
-                )}
+          {isPopupVisible && <BookaDemoPopUp onClose={handleClosePopup} />}
 
           {/* laptop */}
           <div className="hidden lg:block border border-primary02 rounded-3xl mt-[1.25rem] mx-[2rem] mb-14">
@@ -261,10 +262,10 @@ const ServicePage = () => {
               </div>
 
               <div>
-              <Link href="/enrollnow/aceentranceexams">
-                <div className="cursor-pointer w-[18.75rem] h-[3.5rem] bg-gradient01  border-custom rounded-md flex items-center justify-center">
-                  <button className="text-white">Enroll Now</button>
-                </div>
+                <Link href="/enrollnow/aceentranceexams">
+                  <div className="cursor-pointer w-[18.75rem] h-[3.5rem] bg-gradient01  border-custom rounded-md flex items-center justify-center">
+                    <button className="text-white">Enroll Now</button>
+                  </div>
                 </Link>
 
                 <div className="flex justify-center items-center py-2">
@@ -275,12 +276,13 @@ const ServicePage = () => {
                   <div className="bg-[#00000015] w-[120px] h-[1px]"></div>
                 </div>
                 <u className="text-primary02 text-center cursor-pointer">
-                  <h1 className=""  onClick={handleClick}>Contact Us</h1>
+                  <h1 className="" onClick={handleClick}>
+                    Contact Us
+                  </h1>
                 </u>
               </div>
             </div>
           </div>
-
 
           {/* mobile */}
           <div className="lg:hidden border rounded-md px-4 py-6">
@@ -289,14 +291,18 @@ const ServicePage = () => {
                 Want to prepare for top Boarding School ?
               </h1>
               <div className="flex justify-between px-2 pt-4">
-              <Link href="/enrollnow/aceentranceexams">
-                <div className="cursor-pointer w-[7.5rem] h-[1.9rem]  bg-gradient01  border-custom rounded-md flex items-center justify-center text-[0.875rem] text-white ">
-                  Enroll Now
-                </div></Link>
+                <Link href="/enrollnow/aceentranceexams">
+                  <div className="cursor-pointer w-[7.5rem] h-[1.9rem]  bg-gradient01  border-custom rounded-md flex items-center justify-center text-[0.875rem] text-white ">
+                    Enroll Now
+                  </div>
+                </Link>
                 <div className="text-[#00000080]  text-[0.875rem] pt-[5px]">
                   OR
                 </div>
-                <u className="text-primary02 cursor-pointer  text-[0.875rem] pt-[5px]" onClick={handleClick}>
+                <u
+                  className="text-primary02 cursor-pointer  text-[0.875rem] pt-[5px]"
+                  onClick={handleClick}
+                >
                   Contact Us
                 </u>
               </div>
